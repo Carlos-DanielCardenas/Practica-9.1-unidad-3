7f4d54a26c8ed997285c1a4a93830d8e3e40c0f9
CARLOS DANIEL CARDENAS MALPICA <L23580450@reynosa.tecnm.mx>
2023-11-20 12:27:02 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Practica_9__unidad_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            listBox1 = new ListBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(76, 70);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(210, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Factorial de un numero";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(294, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(151, 33);
+            textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(325, 121);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(120, 154);
+            listBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(39, 298);
+            label2.Name = "label2";
+            label2.Size = new Size(197, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Factorial del Numero ";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(242, 295);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 33);
+            textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(366, 303);
+            label3.Name = "label3";
+            label3.Size = new Size(30, 25);
+            label3.TabIndex = 5;
+            label3.Text = "Es";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(427, 295);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 33);
+            textBox3.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Eras Demi ITC", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(510, 131);
+            button1.Name = "button1";
+            button1.Size = new Size(132, 37);
+            button1.TabIndex = 7;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1257, 749);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(listBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(5, 5, 5, 5);
+            Name = "Form1";
+            Text = "Factorial de un numero";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+        private Button button1;
+    }
+}
